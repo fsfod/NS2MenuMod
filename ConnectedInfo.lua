fd82c29b7e6af5efc1e26236f2304060cdc774e3
fsfod <z>
2012-02-09 16:49:56 +0000

    Removed now redundant ScoreboardServerInfo

--- ConnectedInfo.lua
+++ ConnectedInfo.lua
@@ -38,8 +38,6 @@ function ConnectedInfo:OnClientLoadComplete()
     
     self.ConnectedAddress = serverInfo.Address
   end
-  
-  GameGUIManager:CreateFrame("ScoreboardServerInfo")
 end
 
 function ConnectedInfo:SetServerInfo(serverInfo) 
@@ -110,38 +108,3 @@ end
 
 Event.Hook("ClientDisconnected", ConnectedInfo.Disconnected)
 Event.Hook("ClientConnected", ConnectedInfo.Connected)
-
-ControlClass("ScoreboardServerInfo", BaseControl)
-
-function ScoreboardServerInfo:Initialize()
-  BaseControl.Initialize(self, 100, 20)
-  
-  self:SetupHitRec()
-  
-  self:SetPoint("Top", 100, 35, "Top")
-  
-  self:SetColor(Color(0, 0, 0, 0))
-
-  local text = string.format("Server: %s", ConnectedInfo:GetConnectedServerName())
-
-  local info = self:CreateFontString(20, "Center")
-   info:SetText(text)
-   info:SetIsVisible(false)
-   info:SetTextAlignmentX(GUIItem.Align_Center)
-   info:SetTextAlignmentX(GUIItem.Align_Center)
-  self.Info = info
-  
-  self.InfoShown = false
-end
-
-function ScoreboardServerInfo:Update()
-  local showInfo = ScoreboardUI_GetVisible()
-  
-  if(showInfo and not self.InfoShown) then
-    self.Info:SetIsVisible(true)
-    self.InfoShown = true
-  elseif(not showInfo and self.InfoShown) then
-    self.Info:SetIsVisible(false)
-    self.InfoShown = false
-  end
-end
