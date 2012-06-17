a2067e4de80f2e8ef6a3a5774ce925dba1b235a5
fsfod <z>
2012-06-17 14:46:28 +0100

    KeybindPage: Added support for setting secondary keys for binds also redesigned binding buttons

--- KeybindPage.lua
+++ KeybindPage.lua
@@ -36,10 +36,130 @@ local ModifierKeys ={
   
 HotReload = KeybindListEntry
 
+ControlClass("KeybindButton", BaseControl)
+
+KeybindButton.NonActiveColor = Color(0.15,0.15, 0, 0.8)
+KeybindButton.BindModeColor = Color(0.8666, 0.3843, 0, 1)
+KeybindButton.FontSize = 20
+
+function KeybindButton:Initialize(width, height, keyIndex)
+  BaseControl.Initialize(self, width, height)
+
+	self:SetColor(self.NonActiveColor)
+
+  self.KeyIndex = keyIndex
+
+	local boundKey = self:CreateFontString(self.FontSize)
+   boundKey:SetPosition(Vector(10, 0, 0))
+   boundKey:SetAnchor(GUIItem.Left, GUIItem.Center)
+   boundKey:SetTextAlignmentX(GUIItem.Align_Min)
+   boundKey:SetTextAlignmentY(GUIItem.Align_Center)
+  self.BoundKey = boundKey
+end
+
+function KeybindButton:DataSet(data)
+
+  self.Selected = false
+
+  if(data.Keybinds) then
+    self:Hide()
+   return
+  else
+    self:Show()
+    self:SetColor(self.NonActiveColor)
+  end
+
+  GUIMenuManager:ClearFocusIfFrame(self)
+
+  self.BindName = data[1]
+
+  local keyname = KeyBindInfo:GetBoundKey(self.BindName, self.KeyIndex) or ""
+  self.BoundKey:SetText(FriendlyNames[keyname] or keyname)
+  
+  if(KeyBindInfo.BindConflicts[self.BindName] and self.KeyIndex == 1) then
+    self:SetColor(Color(1, 0, 0, 1))
+  end
+end
+
+function KeybindButton:ExitBindingMode()
+	self:SetColor(self.NonActiveColor)
+	self.InBindMode = false
+	
+	local keyname = KeyBindInfo:GetBoundKey(self.BindName, self.KeyIndex) or ""
+	self.BoundKey:SetText(FriendlyNames[keyname] or keyname )
+end
+
+
+function KeybindButton:OnFocusLost()
+  if(self.InBindMode) then
+    self:ExitBindingMode()
+  end
+  
+  self.Selected = false
+  
+  self:SetColor(self.NonActiveColor)
+end
+
+local lastModifer
+
+function KeybindButton:SendKeyEvent(key, down)
+
+  if(self.InBindMode and (down or key == InputKey.MouseZ) and key ~= InputKey.MouseX and key ~= InputKey.MouseY) then
+
+    local currentKey, groupName = KeyBindInfo:GetBindinfo(self.BindName)
+
+    if(ModifierKeys[key] and groupName and string.find(groupName, "Commander")) then
+      lastModifer = key
+     return
+    end
+    
+    self:SetBind(key, down)
+   
+    return true
+  end
+end
+
+function KeybindButton:SetBind(key, down)
+  
+  self.Parent.Parent.Parent:SetKeybind(self.BindName, key, down, self.KeyIndex)
+   
+  self:ExitBindingMode()
+   
+  self:GetGUIManager():ClearFocus()
+end
+
+function KeybindButton:OnClick(button, down, x, y)
+
+  if(self.InBindMode and down) then
+    self:SetBind(button)
+    
+    return true
+  end
+
+  if(down and button == InputKey.MouseButton0 and not self.Parent.Data.Keybinds) then
+    
+    if(self.LastClicked and (Client.GetTime()-self.LastClicked) < GUIMenuManager.DblClickSpeed) then
+       self.InBindMode = true
+       self:SetColor(Color(0.8666, 0.3843, 0, 1))
+       
+       GUIMenuManager:SetFocus(self)
+      return true
+    else
+      self:SetColor(ControlGrey2) 
+      self.Parent.Parent.Parent:SetSelectedButton(self)
+    end
+    
+    self.LastClicked = Client.GetTime()
+  end
+
+  //return false so the listview will select our entry
+  return false
+end
+
 ControlClass('KeybindListEntry', BaseControl)
 
-KeybindListEntry.FontSize = 20
-KeybindListEntry.KeyNameOffset = 300
+KeybindListEntry.FontSize = 24
+KeybindListEntry.KeyNameOffset = 280
 
 function KeybindListEntry:Initialize(owner, width, height)
   BaseControl.Initialize(self, width, height)
@@ -49,28 +169,24 @@ function KeybindListEntry:Initialize(owner, width, height)
   self:SetColor(1,1,1,0)
 
   local name = self:CreateFontString(self.FontSize)
-   name:SetPosition(Vector(38, 0, 0))
+   name:SetPosition(Vector(28, 0, 0))
    //name:SetAnchor(GUIItem.Left, GUIItem.Center)
    name:SetTextAlignmentX(GUIItem.Align_Min)
    name:SetTextAlignmentY(GUIItem.Align_Min)
   self.KeybindName = name
 
-  local bindModeOverlay = self:CreateGUIItem()
-    bindModeOverlay:SetSize(Vector(120, 20, 0))
-    bindModeOverlay:SetPosition(Vector(self.KeyNameOffset-10, 0, 0))
-    bindModeOverlay:SetIsVisible(false)
-	  bindModeOverlay:SetColor(Color(0.8666, 0.3843, 0, 1))
-  self.BindModeOverlay = bindModeOverlay
-
-  local boundKey = self:CreateFontString(self.FontSize)
-   boundKey:SetPosition(Vector(self.KeyNameOffset, 0, 0))
-   //boundKey:SetAnchor(GUIItem.Left, GUIItem.Center)
-   boundKey:SetTextAlignmentX(GUIItem.Align_Min)
-   boundKey:SetTextAlignmentY(GUIItem.Align_Min)
-  self.BoundKey = boundKey
+  local key1 = self:CreateControl("KeybindButton", 140, height, 1)
+    key1:SetPosition(Vector(self.KeyNameOffset-10, 0, 0))
+    self:AddChild(key1)
+  self.Key1 = key1
   
+  local key2 = self:CreateControl("KeybindButton", 140, height, 2)
+    key2:SetPosition(Vector(self.KeyNameOffset+150, 0, 0))
+    self:AddChild(key2)
+  self.Key2 = key2
+
   local groupLabel = self:CreateFontString(self.FontSize)
-   groupLabel:SetPosition(Vector(8, 0, 0))
+   groupLabel:SetPosition(Vector(10, 0, 0))
    groupLabel:SetColor(Color(0.8666, 0.3843, 0, 1))
    groupLabel:SetTextAlignmentX(GUIItem.Align_Min)
    groupLabel:SetTextAlignmentY(GUIItem.Align_Min)
@@ -86,132 +202,51 @@ function KeybindListEntry:SetWidth(width)
   self:SetSize(width, self:GetHeight())
 end
 
-//self.SelectBG:SetIsVisible(true)
-
-function KeybindListEntry:OnFocusLost()
-  if(self.InBindMode) then
-    self:ExitBindingMode()
-  end
-end
-
-local lastModifer
-
-function KeybindListEntry:SendKeyEvent(key, down)
-
-  if(self.InBindMode and (down or key == InputKey.MouseZ) and key ~= InputKey.MouseX and key ~= InputKey.MouseY) then
-
-    local currentKey, groupName = KeyBindInfo:GetBindinfo(self.Data[1])
-
-    if(ModifierKeys[key] and groupName and string.find(groupName, "Commander")) then
-      lastModifer = key
-     return
-    end
-    
-    self:SetBind(key, down)
-   
-    return true
-  end
-end
-
-function KeybindListEntry:SetBind(newKey, down)
-  self.Parent.Parent:SetKeybind(self.Data[1], newKey, down)
-  self:ExitBindingMode()
-
-  self.Parent:GetGUIManager():ClearFocus()
-end
-  
-
-function KeybindListEntry:OnClick(button, down, x, y)
-
-  if(self.InBindMode and down) then
-    self:SetBind(button)
-    
-    return true
-  end
-
-  if(down and button == InputKey.MouseButton0 and not self.Data.Keybinds) then
-    
-    if(x < 300) then
-      return false
-    end
-    
-    if(self.LastClicked and (Client.GetTime()-self.LastClicked) < GUIMenuManager.DblClickSpeed) then
-       self.InBindMode = true
-       self.BindModeOverlay:SetColor(Color(0.8666, 0.3843, 0, 1))
-       self.BindModeOverlay:SetIsVisible(true)
-       
-       GUIMenuManager:SetFocus(self)
-      return true
-    end
-    
-    self.LastClicked = Client.GetTime()
-  end
-
-  //return false so the listview will select our entry
-  return false
-end
-
-function KeybindListEntry:ExitBindingMode()
-	self.BindModeOverlay:SetIsVisible(false)
-	self.InBindMode = false
-	
-	local keyname = KeyBindInfo:GetBoundKey(self.Data[1]) or ""
-	self.BoundKey:SetText(FriendlyNames[keyname] or keyname )
-end
-
 function KeybindListEntry:SetData(data)
   if(self.Hidden) then
     self.Background:SetIsVisible(true)
     self.Hidden = nil
   end
 
-  self.Data = data
+  self.Data = data 
 
-  self.BindModeOverlay:SetIsVisible(false)
+  self.Key1:DataSet(data)
+  self.Key2:DataSet(data)
 
   if(not data.Keybinds) then
     self.KeybindName:SetText(data[2])
-   
-    local keyname = KeyBindInfo:GetBoundKey(data[1]) or ""
-    self.BoundKey:SetText(FriendlyNames[keyname] or keyname )
-    
-    if(KeyBindInfo.BindConflicts[data[1]]) then
-      self.BindModeOverlay:SetColor(Color(1, 0, 0, 1))
-      self.BindModeOverlay:SetIsVisible(true)
-    end
-    
     self.GroupLabel:SetText("")
   else
-    self.BoundKey:SetText("")
     self.KeybindName:SetText("")
-    self.BindModeOverlay:SetIsVisible(false)
-    
     self.GroupLabel:SetText(data.Name)
   end
 
 end
 
+
 ControlClass('KeybindPage', BasePage)
 
 KeybindPage.ListSetup = {
-  Width = 500,
-  Height = 400,
-  ItemHeight = 20,
+  Width = 600,
+  Height = 500,
+  ItemHeight = 26,
   ItemSpacing = 4,
   ItemClass = "KeybindListEntry",
   ScrollBarWidth = 23,
+  ItemsSelectable = false,
 }
 
 function KeybindPage:Initialize()
-  BasePage.Initialize(self, 600, 500, "Keybinds")
+  BasePage.Initialize(self, 700, 600, "Keybinds")
   BaseControl.Hide(self)
 
   assert(KeyBindInfo, "Keybinds mod is not loaded")
 
+
   KeyBindInfo:Init(true)
 
   local keybindList = self:CreateControl("ListView", self.ListSetup)
-    keybindList:SetPoint("Center", 0, -40, "Center")
+    keybindList:SetPoint("Top", 0, 30, "Top")
     keybindList.RootFrame:SetColor(Color(0, 0, 0, 1))
     keybindList:SetDataList(KeyBindInfo:GetBindingDialogTable())    
     self:AddChild(keybindList)
@@ -220,6 +255,7 @@ function KeybindPage:Initialize()
   local clearButton = self:CreateControl("UIButton", "Clear Key")
     clearButton:SetPoint("BottomLeft", 120, -15, "BottomLeft")
     clearButton.ClickAction = {self.ClearBind, self}
+    self.ClearButton = clearButton
   self:AddChild(clearButton)
 
 /*
@@ -243,7 +279,7 @@ function KeybindPage:Initialize()
   self.WarningString = warningString
 end
 
-function KeybindPage:SetKeybind(BindName, key, down)
+function KeybindPage:SetKeybind(BindName, key, down, keyIndex)
 
   if(key ~= InputKey.Escape) then
     key = InputKeyHelper:ConvertToKeyName(key, down)
@@ -256,19 +292,27 @@ function KeybindPage:SetKeybind(BindName, key, down)
       self.WarningString:SetText("")
     end
     
-    KeyBindInfo:SetKeybind(key, BindName)
+    KeyBindInfo:SetKeybind(key, BindName, keyIndex)
     
     self:SetKeybindsChanged()
 
   end
 end
 
-function KeybindPage:ClearBind()
-  local bindinfo = self.KeybindList:GetSelectedIndexData()
-   
-  if(bindinfo and not bindinfo.Keybinds) then
-    KeyBindInfo:ClearBind(bindinfo[1])
+function KeybindPage:SetSelectedButton(button)
+  
+  if(self.SelectedButton) then
+    self.SelectedButton.Selected = false
+  end
+  
+  self.SelectedButton = button
+  button.Selected = true
+end
 
+function KeybindPage:ClearBind()
+
+  if(self.SelectedButton and self.SelectedButton.Selected) then  
+    KeyBindInfo:ClearBind(self.SelectedButton.BindName, self.SelectedButton.KeyIndex)
     self:SetKeybindsChanged()
   end
 end
